66c253d59c5e72697658b7316a0e0f79668aea8e
satrajeet2006 <satrajeetmandal@gmail.com>
2026-02-11 18:10:33 +0000

    your message here

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS as newer version
+
+// javascript engine's is inside the browser
+
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3 
+    +
+    3) // code readability should be high
+
+let name = "SAtty"
+let
