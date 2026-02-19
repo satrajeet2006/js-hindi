0b559db0233b7b094cd3579a285d3ae754ec848e
satrajeet2006 <satrajeetmandal@gmail.com>
2026-02-19 19:48:25 +0000

    operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -22,6 +22,48 @@ let booleanloggedin = Boolean(loggedin);
 
 let x = 33;
 let stringX = String(x);
-console.log(x);
-console.log(typeof x);
+//console.log(x);
+//console.log(typeof x);
+
+// ********************** OPERATIONS **********************
+
+let value = 3;
+let negvalue = -value;
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3); // power
+
+let str1 = "satty"
+let str2 = "qt"
+
+let str3 = str1 + " " + str2
+//console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1 + "2" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//console.log( (3 + 4) * 5 % 3)
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
 
