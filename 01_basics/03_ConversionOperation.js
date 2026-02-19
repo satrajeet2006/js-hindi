993248cba3e886a31be941ae80dd479b6293bc13
satrajeet2006 <satrajeetmandal@gmail.com>
2026-02-19 19:30:23 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+// null => 0
+// undefined => NaN
+// 33abc => NaN
+// true => 1, false => 0 (boolean)
+//"" => false
+//"nonEmptyString" => true
+
+let loggedin = "satty";
+let booleanloggedin = Boolean(loggedin);
+//console.log(booleanloggedin);
+
+let x = 33;
+let stringX = String(x);
+console.log(x);
+console.log(typeof x);
+
